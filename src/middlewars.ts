cb7e2609be78f5b9fd3e0a483d012326c9c46d49
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-21 19:18:36 -0300

    feat: criando função de verificação de erros na requisição post.
    Verificação de email e name, se os campos nescessários foram preenchidos e se o email já é existente.

--- src/middlewars.ts
+++ src/middlewars.ts
@@ -1,2 +1,31 @@
 import { Request, Response, NextFunction } from "express";
 import { client } from "./database";
+import format from "pg-format";
+
+const validateNewDeveloper = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const payload = req.body;
+
+  if (Object.keys(payload).length !== 2 || !payload.name || !payload.email) {
+    return res.status(422).json({ message: "Invalid payload" });
+  }
+
+  const queryString = format(
+    `
+    SELECT * FROM developers WHERE email = %L;
+    `,
+    payload.email
+  );
+
+  const queryResult = await client.query(queryString);
+  if (queryResult.rows.length) {
+    return res.status(409).json({ error: "Email already in use" });
+  }
+
+  next();
+};
+
+export { validateNewDeveloper };
