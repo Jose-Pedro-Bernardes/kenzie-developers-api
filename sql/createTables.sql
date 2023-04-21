ae99fb56be6e6015be01a39558d1cd6873fa3ce8
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-21 12:17:19 -0300

    feat: tabelas do banco de dados criadas.

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -5,6 +5,17 @@ CREATE TABLE IF NOT EXISTS developers (
   PRIMARY KEY(id)
 );
 
+CREATE TABLE IF NOT EXISTS developers_info(
+  id                    SERIAL                                      NOT NULL, 
+  developerSince        DATE                                        NOT NULL,
+  preferredOS           os_type                                     NOT NULL,
+  developerId           INTEGER     REFERENCES developers(id)       UNIQUE NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TYPE os_type AS ENUM ('Windows', 'Linux', 'MacOS');
+
+
 CREATE TABLE IF NOT EXISTS projects (
   id                SERIAL        NOT NULL,
   name              VARCHAR(50)   NOT NULL,
@@ -16,3 +27,18 @@ CREATE TABLE IF NOT EXISTS projects (
   developerId       INTEGER       REFERENCES developers(id),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE IF NOT EXISTS technologies (
+  id        SERIAL        NOT NULL,
+  name      VARCHAR(50)   NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS projects_technologies (
+  id              SERIAL                                    NOT NULL,
+  addedIn         DATE                                      NOT NULL,
+  technologyId    INTEGER    REFERENCES technologies(id)    NOT NULL,
+  projectId       INTEGER    REFERENCES projects(id)        NOT NULL,
+  PRIMARY KEY(id)
+);
+
