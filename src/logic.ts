eb7ce1f693c863662886cfdde3d1312fa14e34d4
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-21 17:05:29 -0300

    feat: adicionar função registerNewDeveloper() para enviar a resposta da requisição de criação de desenvolvedor com status HTTP 201 e dados do desenvolvedor criado.

--- src/logic.ts
+++ src/logic.ts
@@ -1,1 +1,53 @@
 import { Response, Request } from "express";
+import format from "pg-format";
+import { client } from "./database";
+
+const registerNewDeveloper = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const registerEntrie = req.body;
+
+  const queryString = format(
+    `
+    INSERT INTO developers(%I)
+    VALUES(%L)
+    RETURNING *;
+  `,
+    Object.keys(registerEntrie),
+    Object.values(registerEntrie)
+  );
+
+  const queryResult = await client.query(queryString);
+  const createdDeveloper = queryResult.rows[0];
+
+  return res.status(201).json(createdDeveloper);
+};
+
+const listDeveloperById = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(200).json();
+};
+
+const updateDeveloperData = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(200).json();
+};
+
+const removeDeveloper = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(204).json();
+};
+
+const registerAdicionalInfo = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(201).json();
+};
