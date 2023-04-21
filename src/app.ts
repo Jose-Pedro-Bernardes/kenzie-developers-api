e43a6126868ab8f1f0ce9fe92f49ca157a8399e4
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-21 19:14:49 -0300

    feat: adicionando as funções de validação e de criação de developer.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application, json } from "express";
 import "dotenv/config";
+import { validateNewDeveloper } from "./middlewars";
+import { registerNewDeveloper } from "./logic";
 
 const app: Application = express();
 
 app.use(json());
 
-app.post("/developers");
+app.post("/developers", validateNewDeveloper, registerNewDeveloper);
 app.get("developers/:id");
 app.patch("developers/:id");
 app.delete("developers/:id");
