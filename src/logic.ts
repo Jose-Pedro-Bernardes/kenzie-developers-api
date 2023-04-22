8dc1a1c1929b5ccadf66eb8bdb068b993f949eb5
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-21 23:42:39 -0300

    feat: criado função de retornar um novo objeto com as informações do desenvolvedor pesquisado e retornando um status code 200.

--- src/logic.ts
+++ src/logic.ts
@@ -1,7 +1,12 @@
 import { Response, Request } from "express";
 import format from "pg-format";
 import { client } from "./database";
-import { developerCreate, developerResult } from "./interfaces";
+import {
+  IListDeveloperRes,
+  developerCreate,
+  developerInfoResult,
+  developerResult,
+} from "./interfaces";
 
 const registerNewDeveloper = async (
   req: Request,
@@ -29,7 +34,31 @@ const listDeveloperById = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  return res.status(200).json();
+  const id: number = parseInt(req.params.id);
+
+  const queryString = format(
+    `
+  SELECT * FROM developers WHERE id = %L;
+  `,
+    id
+  );
+  const queryResult: developerResult = await client.query(queryString);
+  const developer = queryResult.rows[0];
+
+  const queryString2 = format(
+    `
+  
+  SELECT developerSince, preferredOS FROM developers_Info WHERE developerId = %L;
+  
+  `,
+    id
+  );
+  const queryResult2: developerInfoResult = await client.query(queryString2);
+  const developerInfo = queryResult2.rows[0];
+
+  const developerRes = { ...developer, ...developerInfo };
+
+  return res.status(200).json(developerRes);
 };
 
 const updateDeveloperData = async (
