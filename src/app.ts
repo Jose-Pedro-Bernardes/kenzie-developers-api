a246d21804fbde62e0ca7acc9498284212b6d702
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-21 23:41:15 -0300

    feat: adicionando a função middleware e a função responsável pela lógica da resposta HTTP 200.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express, { Application, json } from "express";
 import "dotenv/config";
-import { validateNewDeveloper } from "./middlewars";
-import { registerNewDeveloper } from "./logic";
+import { idDeveloperVerification, validateNewDeveloper } from "./middlewars";
+import { listDeveloperById, registerNewDeveloper } from "./logic";
 
 const app: Application = express();
 
 app.use(json());
 
 app.post("/developers", validateNewDeveloper, registerNewDeveloper);
-app.get("developers/:id");
+app.get("developers/:id", idDeveloperVerification, listDeveloperById);
 app.patch("developers/:id");
 app.delete("developers/:id");
 app.post("/developers/:id/infos");
