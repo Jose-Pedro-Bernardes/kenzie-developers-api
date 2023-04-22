9c307fb2c021aae07e986f61700c10fc1dcf639f
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-21 23:41:48 -0300

    feat: criar interfaces para função de buscar usuario pelo id.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -9,4 +9,28 @@ interface IDeveloper {
 type developerCreate = Omit<IDeveloper, "id">;
 type developerResult = QueryResult<IDeveloper>;
 
-export { IDeveloper, developerCreate, developerResult };
+interface IListDeveloperRes {
+  developerId: number;
+  developerName: string;
+  developerEmail: string;
+  developerInfoDeveloperSince: Date | null;
+  developerInfoPreferredOS: string | null;
+}
+
+interface IDeveloperInfo {
+  id: number;
+  developerSince: Date;
+  preferredOS: "Windows" | "Linux" | "MacOS";
+  developerId: number;
+}
+
+type developerInfoOmit = Omit<IDeveloperInfo, "id" | "developerId">;
+type developerInfoResult = QueryResult<developerInfoOmit>;
+
+export {
+  IDeveloper,
+  developerCreate,
+  developerResult,
+  developerInfoResult,
+  IListDeveloperRes,
+};
