10c8f8cddb0566b923a3411fd83bcefbbfc8bc78
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-22 13:02:48 -0300

    feat: adicionando lógica da rota "/developers/:id" do tipo patch.

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,40 @@
 import express, { Application, json } from "express";
 import "dotenv/config";
-import { idDeveloperVerification, validateNewDeveloper } from "./middlewars";
-import { listDeveloperById, registerNewDeveloper } from "./logic";
+import {
+  idDeveloperVerification,
+  reqValidation,
+  validateNewDeveloper,
+} from "./middlewars";
+import {
+  listDeveloperById,
+  registerAdicionalInfo,
+  registerNewDeveloper,
+  removeDeveloper,
+  updateDeveloperData,
+} from "./logic";
 
 const app: Application = express();
 
 app.use(json());
 
-app.post("/developers", validateNewDeveloper, registerNewDeveloper);
-app.get("developers/:id", idDeveloperVerification, listDeveloperById);
-app.patch("developers/:id");
-app.delete("developers/:id");
-app.post("/developers/:id/infos");
+app.post(
+  "/developers",
+  reqValidation,
+  validateNewDeveloper,
+  registerNewDeveloper
+);
+app.get("/developers/:id", idDeveloperVerification, listDeveloperById);
+app.patch(
+  "/developers/:id",
+  reqValidation,
+  idDeveloperVerification,
+  updateDeveloperData
+);
+app.delete("/developers/:id", idDeveloperVerification, removeDeveloper);
+app.post(
+  "/developers/:id/infos",
+  idDeveloperVerification,
+  registerAdicionalInfo
+);
 
 export default app;
