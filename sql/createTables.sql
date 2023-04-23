9f5704e90803964c311ba6f0a86dd38bca6d3fa2
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-23 17:10:14 -0300

    fix: corrigindo erros de indentação que estava causando bugs.

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,41 +1,43 @@
 CREATE TABLE IF NOT EXISTS developers (
-  id      SERIAL        NOT NULL,
-  name    VARCHAR(50)   NOT NULL,
-  email   VARCHAR(50)   UNIQUE NOT NULL,
-  PRIMARY KEY(id)
+  "id"      SERIAL        NOT NULL,
+  "name"    VARCHAR(50)   NOT NULL,
+  "email"   VARCHAR(50)   UNIQUE NOT NULL,
+  PRIMARY KEY("id")
 );
 
-CREATE TYPE os_type AS ENUM ('Windows', 'Linux', 'MacOS');
+CREATE TYPE "OS" AS ENUM ('Windows', 'Linux', 'MacOS');
 
 
-CREATE TABLE IF NOT EXISTS developers_info(
-  id                    SERIAL                                      NOT NULL, 
-  developerSince        DATE                                        NOT NULL,
-  preferredOS           os_type                                     NOT NULL,
-  developerId           INTEGER     REFERENCES developers(id)       UNIQUE NOT NULL,
-  PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS developer_infos (
+  "id"                          SERIAL                           NOT NULL, 
+  "developerSince"              DATE                             NOT NULL,
+  "preferredOS"                 "OS"                             NOT NULL,
+  "developerId"                 INTEGER                          UNIQUE NOT NULL,
+  FOREIGN KEY ("developerId")   REFERENCES developers(id)        ON DELETE CASCADE,
+  PRIMARY KEY("id")
 );
 
 
 CREATE TABLE IF NOT EXISTS projects (
-  id                SERIAL        NOT NULL,
-  name              VARCHAR(50)   NOT NULL,
-  description       TEXT,
-  estimatedTime     VARCHAR(120)  NOT NULL,
-  repository        VARCHAR(120)  NOT NULL,
-  startDate         DATE          NOT NULL,
-  endDate           DATE,
-  developerId       INTEGER       REFERENCES developers(id),
-  PRIMARY KEY(id)
+  "id"                SERIAL        NOT NULL,
+  "name"              VARCHAR(50)   NOT NULL,
+  "description"       TEXT,
+  "estimatedTime"     VARCHAR(120)  NOT NULL,
+  "repository"        VARCHAR(120)  NOT NULL,
+  "startDate"         DATE          NOT NULL,
+  "endDate"           DATE,
+  "developerId"       INTEGER,
+  FOREIGN KEY ("developerId") REFERENCES developers(id) ON DELETE SET NULL,
+  PRIMARY KEY("id")
 );
 
 CREATE TABLE IF NOT EXISTS technologies (
-  id        SERIAL        NOT NULL,
-  name      VARCHAR(50)   NOT NULL,
+  "id"        SERIAL        NOT NULL,
+  "name"      VARCHAR(50)   NOT NULL,
   PRIMARY KEY(id)
 );
 
-INSERT INTO technologies(name)
+INSERT INTO technologies("name")
 VALUES
 ('JavaScript'),
 ('Python'),
@@ -49,10 +51,12 @@ VALUES
 
 
 CREATE TABLE IF NOT EXISTS projects_technologies (
-  id              SERIAL                                    NOT NULL,
-  addedIn         DATE                                      NOT NULL,
-  technologyId    INTEGER    REFERENCES technologies(id)    NOT NULL,
-  projectId       INTEGER    REFERENCES projects(id)        NOT NULL,
-  PRIMARY KEY(id)
+  "id"              SERIAL                                    NOT NULL,
+  "addedIn"         DATE                                      NOT NULL,
+  "technologyId"    INTEGER                                   NOT NULL,
+  "projectId"       INTEGER                                   NOT NULL,
+  FOREIGN KEY ("technologyId")    REFERENCES technologies(id) ON DELETE SET NULL,
+  FOREIGN KEY ("projectId")       REFERENCES projects(id)     ON DELETE CASCADE,
+  PRIMARY KEY("id")
 );
 
